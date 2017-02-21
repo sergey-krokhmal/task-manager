1d6e57a2bee08a2f258f5da58e2ad45f44d2d1bc
sergey-krokhmal <xai.sergey.krokhmal@gmail.com>
2017-02-21 23:15:42 +0300

    Add database drivers

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
-use Krokhmal\Soft\Tasker\Test;
 use Krokhmal\Soft\Helpers\UUID;
+use Krokhmal\Soft\Tasker\Test;
+use Krokhmal\Soft\Tasker\MysqlTaskRepository;
+use Krokhmal\Soft\Data\Database\DbConfig;
 
+// Добавить автозагрузчик
 $loader = require (__DIR__ . '/vendor/autoload.php');
-
+// Установка соответсвия префикса пространства имен с его базовым каталогом
 $loader->addPsr4( 'Krokhmal\\Soft\\', __DIR__ . '/lib/Krokhmal-Soft/');
 
-echo UUID::generateV4();
+var_dump(DbConfig::DB_CONFIG_PARAMS);
+//echo UUID::generateV4();
---
+++ lib/Krokhmal-Soft/Data/Database/DbConfig.php
@@ -0,0 +1,48 @@
+<?php
+namespace Krokhmal\Soft\Data\Database;
+
+class DbConfig
+{
+    // Global variable with main DB parameters for connection
+    const DB_CONFIG_PARAMS = array(
+        
+        // Put here another connection params, like:
+        /*
+        
+        // Name of db connection config
+        'connect_name' => array(
+        
+            // Driver using for this connection ('pdo' or 'mysqli')
+            'driver' => 'pdo',
+            
+            // Connection params for this connection
+            'params' => array(
+                'host'			=>	'hostname',
+                'db_name'		=>	'db_name',
+                'user'			=>	'db_user',
+                'pass'			=>	'password',
+                'persistant'	=>	true	// Persistant connection
+            )
+        )
+        
+        */
+        
+        // Name of db connection config
+        'db' => array(
+        
+            // Driver using for this connection
+            'driver' => 'pdo',
+            
+            // Connection params for this connection
+            'params' => array(
+                'host'			=>	'localhost',
+                'db_name'		=>	'test',
+                'user'			=>	'sergey',
+                'pass'			=>	'8kowalsky8',
+                'charset'		=>	'utf8',
+                'persistant'	=>	true
+            )
+        )
+    );
+}
+ 
