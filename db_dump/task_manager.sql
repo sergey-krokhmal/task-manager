f016eca3b3303e1975e8642cfdd1090ee11a3dee
sergey-krokhmal <xai.sergey.krokhmal@gmail.com>
2017-03-01 02:39:20 +0300

    New mysql_dump

--- db_dump/task_manager.sql
+++ db_dump/task_manager.sql
@@ -1,11 +1,30 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.7
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Мар 01 2017 г., 02:28
+-- Версия сервера: 5.7.13
+-- Версия PHP: 7.0.8
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- База данных: `task_manager`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `task`
+--
 
 CREATE TABLE IF NOT EXISTS `task` (
   `id` int(10) unsigned NOT NULL,
@@ -14,15 +33,23 @@ CREATE TABLE IF NOT EXISTS `task` (
   `id_priority` int(10) unsigned NOT NULL DEFAULT '1',
   `id_status` int(10) unsigned NOT NULL DEFAULT '1',
   `tags` varchar(256) NOT NULL DEFAULT ''
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `task`
+--
 
 INSERT INTO `task` (`id`, `uuid`, `name`, `id_priority`, `id_status`, `tags`) VALUES
-(1, '69cd086c-8ff8-4436-afdf-024756ed7bc7', 'Тестовая задача', 2, 1, 'tag1|tag2|tag5'),
-(2, 'd299355b-ade7-48eb-97e7-d4d461b88fe0', 'test', 2, 2, 'Протестировать|Познать дзен'),
-(3, '69ef1300-1692-4392-b959-7020ef13d8ca', 'Вава', 1, 1, 'Вава|Бубу|Кукууу|test'),
-(5, 'd2a3a510-0c9b-4605-b51a-d0097bd5a7c9', 'ааа', 2, 2, 'tag1|tag2|tag3|Протестировать|Познать дзен|Вава|Бубу|Кукууу'),
-(8, '3ccc87bf-c02d-460f-9a98-4631f10ab587', 'ккк', 1, 1, 'test'),
-(15, 'eac48ac2-d215-484b-9005-ad987df2c7d7', 'y', 1, 1, '');
+(1, '933f969d-98e7-4158-9368-cabecbc3f400', 'Новая задача', 2, 2, ''),
+(2, '1d543188-c552-4219-9951-c9ca2b265140', 'Написать задачник', 3, 1, ''),
+(3, '0ef59550-26e8-495c-b98e-7c22ab70ff01', 'Убраться в квартире', 1, 2, ''),
+(4, '896c372d-bf00-4089-9e17-59ccb293af38', 'Купить продукты', 1, 1, '');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `task_priority`
+--
 
 CREATE TABLE IF NOT EXISTS `task_priority` (
   `id` int(10) unsigned NOT NULL,
@@ -30,10 +57,20 @@ CREATE TABLE IF NOT EXISTS `task_priority` (
   `name` varchar(32) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
+--
+-- Дамп данных таблицы `task_priority`
+--
+
 INSERT INTO `task_priority` (`id`, `value`, `name`) VALUES
 (1, 'low', 'Низкий'),
 (2, 'middle', 'Средний'),
 (3, 'high', 'Высокий');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `task_status`
+--
 
 CREATE TABLE IF NOT EXISTS `task_status` (
   `id` int(10) unsigned NOT NULL,
@@ -41,25 +78,53 @@ CREATE TABLE IF NOT EXISTS `task_status` (
   `name` varchar(32) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
+--
+-- Дамп данных таблицы `task_status`
+--
+
 INSERT INTO `task_status` (`id`, `value`, `name`) VALUES
 (1, 'active', 'В работе'),
 (2, 'done', 'Завершена');
 
+--
+-- Индексы сохранённых таблиц
+--
 
+--
+-- Индексы таблицы `task`
+--
 ALTER TABLE `task`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Индексы таблицы `task_priority`
+--
 ALTER TABLE `task_priority`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Индексы таблицы `task_status`
+--
 ALTER TABLE `task_status`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
 
+--
+-- AUTO_INCREMENT для таблицы `task`
+--
 ALTER TABLE `task`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT для таблицы `task_priority`
+--
 ALTER TABLE `task_priority`
   MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT для таблицы `task_status`
+--
 ALTER TABLE `task_status`
   MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
